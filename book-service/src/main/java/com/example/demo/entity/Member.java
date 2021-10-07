f38f26050eaf0e4d591dbecb0852a9d0aedde33c
srivatsan <vatsan_k@hotmail.com>
2021-10-07 15:51:09 +0530

    Created Member Entity Class

---
+++ book-service/src/main/java/com/example/demo/entity/Member.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Member {
+
+	int memeberId;
+	String memmberName;
+	long mobileNumber;
+	
+	
+	
+	
+}
