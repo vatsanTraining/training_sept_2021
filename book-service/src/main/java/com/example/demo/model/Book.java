aaaf06353da0176841baf15199805b5b0c7583a9
srivatsan <vatsan_k@hotmail.com>
2021-10-07 12:07:16 +0530

    REQ-102 - Added the Model Class Book with Required Fields

---
+++ book-service/src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Book {
+
+	 int bookId;
+	 String bookName;
+	 String author;
+	 
+	
+}
