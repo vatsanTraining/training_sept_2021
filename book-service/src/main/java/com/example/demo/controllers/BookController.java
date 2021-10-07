1fb6520e0e4f8b3fcdca06430db2fb61327caa13
srivatsan <vatsan_k@hotmail.com>
2021-10-07 12:26:45 +0530

    REQ-104 Created the Book Controller Class as per the requirements

---
+++ book-service/src/main/java/com/example/demo/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.services.BookService;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class BookController {
+
+	
+	@Autowired
+	private BookService service;
+	
+	
+	@GetMapping(path = "/books/{id}")
+	public Book getBookById(@PathVariable("id") int id) {
+		
+		return this.service.getBookById(id);
+	}
+}
