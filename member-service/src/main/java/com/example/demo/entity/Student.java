a1a552a69c3fabcdb3ffd5a81f79306ba97c269d
Chanabasappa <chanu88615@gmail.com>
2021-10-08 10:41:55 +0530

    added student class

---
+++ member-service/src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+public class Student {
+	
+	private int id;
+	private String studentName;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	
+
+}
